3cf65163abe53c9bdd56c64f791081bccf2414d8
Adam Kwiatkowski <adam.kwiatkowski256@gmail.com>
2024-10-27 10:37:37 +0100

    Update mode to keep bot reference

--- tomasz/modes/mode.py
+++ tomasz/modes/mode.py
@@ -1,9 +1,10 @@
 from tomasz.map import TomaszMapWithHistory
+from zone_conqueror import MyBot
 
 
 class Mode:
-    def get_priority(self, tomasz_map: TomaszMapWithHistory):
+    def get_priority(self, tomasz_map: TomaszMapWithHistory, my_bot: MyBot):
         pass
 
-    def get_action(self, tomasz_map: TomaszMapWithHistory):
+    def get_action(self, tomasz_map: TomaszMapWithHistory, my_bot: MyBot):
         pass
