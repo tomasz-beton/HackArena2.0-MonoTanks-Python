a7c4c7780792ff98c66208b5995e70945aa0676c
Wojtek Jasiński <wojciech.jasinski@swmansion.com>
2024-10-27 17:31:46 +0100

    huj

--- tomasz/modes/fight_mode.py
+++ tomasz/modes/fight_mode.py
@@ -14,7 +14,11 @@ class FightMode(Mode):
         self.closest_tank = my_bot.alignment.get_closest_enemy()
 
         if self.closest_tank:
-            return 1
+            return min(
+                tomasz_map.agent.entity.turret.bullet_count * 0.5
+                + 1 if tomasz_map.agent.entity.secondary_item else 0,
+                1
+            )
         else:
             return 0
 
@@ -22,4 +26,14 @@ class FightMode(Mode):
         if self.closest_tank:
             my_bot.alignment.set_target(self.closest_tank["pos"])
 
-        return my_bot.alignment.get_action() or AbilityUse(Ability.FIRE_BULLET)
+        alignment_action = my_bot.alignment.get_action()
+        if alignment_action:
+            return alignment_action
+        
+        if tomasz_map.agent.entity.secondary_item:
+            weapon_kurwa = tomasz_map.agent.entity.secondary_item + 1
+            return AbilityUse(weapon_kurwa)
+        elif tomasz_map.agent.entity.turret.bullet_count > 0:
+            return AbilityUse(Ability.FIRE_BULLET)
+        else:
+            return None
